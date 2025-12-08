6b07afb71a2b4d41a2567c6ddaa598b53ae001d1
Abir <malam2331103@bscse.uiu.ac.bd>
2025-12-08 15:56:01 +0600

    firebase .env file add

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBtA9hhxcDjy3rqNMoyL6ue2fZPTQxZQos",
-  authDomain: "digital-life-lessons-145b8.firebaseapp.com",
-  projectId: "digital-life-lessons-145b8",
-  storageBucket: "digital-life-lessons-145b8.firebasestorage.app",
-  messagingSenderId: "389820103082",
-  appId: "1:389820103082:web:9aed017273a7478a3ed3d3"
+   apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
